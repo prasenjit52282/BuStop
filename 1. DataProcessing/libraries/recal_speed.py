5b1ba03d3f207d4af9b76308535572ee2f7b4d84
prasenjit52282 <prasenjitkarmakar52282@gmail.com>
2021-12-12 03:38:43 +0530

    recalculated speed and original speed comparison is added and saved in data processing module

--- 1. DataProcessing/libraries/recal_speed.py
+++ 1. DataProcessing/libraries/recal_speed.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 from geopy.distance import geodesic
 
@@ -12,12 +13,12 @@ def reCalculateSpeed(df_in):
     df=df_in.drop_duplicates('time').copy()
     latlongstime=df[['#lat','long','time']].values
     start_lat,start_long,start_time=latlongstime[0,:]
-    speed=[]
+    speed=[];max_speed=45
     for lat,long,time in latlongstime:
         dis=geodistance((start_lat,start_long),(lat,long)) #distance
         dt=get_Sec_from_datetime(time)-get_Sec_from_datetime(start_time)
         speed_recal=dis/(dt+1e-7)
-        speed.append(speed_recal)
+        speed.append(np.clip(speed_recal,0,max_speed))
 
         start_lat,start_long,start_time=lat,long,time
 
